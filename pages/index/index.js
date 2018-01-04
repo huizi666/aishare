90f500ca91a30980268084c54f59229267b47bc2
huizi666 <huizi_0716@163.com>
2018-01-04 18:20:19 +0800

    增加滚动demo

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,29 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    animationData: {}
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  onShow: function () {
+   
+    var animation = wx.createAnimation({
+      duration: 10000,
+      timingFunction: 'ease-in',
+     
     })
+   
+    this.animation = animation
+    
+    // 先旋转同时放大，然后平移
+    
+    this.rotateThenScale()
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  rotateThenScale: function () {
+    // 先旋转后放大
+    this.animation.translateX(-100).step()
+    this.animation.rotate(45).step()
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      animationData: this.animation.export()
     })
-  }
+  },
+
 })
